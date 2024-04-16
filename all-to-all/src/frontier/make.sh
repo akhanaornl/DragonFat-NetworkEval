38dfb35336f16d7ce5682da5e06ac4b6f2789a27
akhanaornl <167210630+akhanaornl@users.noreply.github.com>
2024-04-16 10:06:11 -0400

    Delete all-to-all directory

--- all-to-all/src/frontier/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ./env
-module -t list
-set -x
-rm -f all
-hipcc -fopenmp -g -O -I${CRAY_MPICH_DIR}/include -o all ../all.cc -L${CRAY_MPICH_DIR}/lib ${PE_MPICH_GTL_DIR_amd_gfx90a} -lmpi ${PE_MPICH_GTL_LIBS_amd_gfx90a}
